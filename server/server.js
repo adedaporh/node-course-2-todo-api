fa9a12a86160dd523a70dd29ab779a3238c868da
Dapo <adedap09@gmail.com>
2017-11-28 22:58:29 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -16,6 +17,22 @@ app.post('/todos', (req, res) => {
         doc => res.send(doc),
         e => res.status(400).send(e)
     );
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Resource not found');
+    }
+
+    Todo.findById(id).then(
+        todo => {
+            if(!todo)
+                return res.status(404).send('Resource not found');
+            res.send(todo);
+        }
+    ).catch(e => res.status(400).send('Hmm... something went wrong'));
 });
 
 app.listen(3000, () => {
