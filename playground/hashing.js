13693d7463d060a6acd59abc4b4ea10cf41fe8b4
Dapo Adediran <adedaporh@outlook.com>
2018-08-30 20:04:28 +0100

    Add hasing middleware

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
-const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+// const {SHA256} = require('crypto-js');
+// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-};
+var password = '123abc!';
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
+
+var hashedPassword = '$2a$10$PC.kv0veEpTjS2MHbUJoAeK7Q9W2bwdOnHfSqCiip8JP7jBTaJbQ6';
+
+bcrypt.compare('123!', hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+// let data = {
+//     id: 10
+// };
+
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
 
 
-let decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
-
+// let decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+ 
 // let message = 'I am user number 3';
 // let hash = SHA256(message).toString();
 
